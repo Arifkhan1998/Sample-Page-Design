7a1c6ea24e5877da2855313f46ba9f6dbddb1e32
Arif Khan <93179229+Arifkhan1998@users.noreply.github.com>
2023-04-22 02:55:37 +0600

    Page Design

---
+++ Custom/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+});
+
+// MOBILE MENU
+function openNav(){
+	document.getElementById("myNav").style.width = "80%";
+}
+
+function closeNav(){
+	document.getElementById("myNav").style.width = "0%";
+}
